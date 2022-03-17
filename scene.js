db6d5b282ba37114bcb0fb1fbc6024a96939db2a
rikingurditta <dolllyamar@gmail.com>
2022-03-17 02:56:07 -0400

    Cleaned up code and added text

--- scene.js
+++ scene.js
@@ -1,103 +1,150 @@
 import * as THREE from './three.module.js';
 import { OrbitControls } from './OrbitControlsJsm.js';
+import { TextGeometry } from './TextGeometry.js';
+import { FontLoader } from './FontLoader.js';
 
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild( renderer.domElement );
+// necessary global objects
+var scene;
+var renderer;
+var camera;
 
-// const spherePosition = new THREE.Vector3(0, 0, 0);
-// const geometry = new THREE.SphereGeometry(0.75, 16, 32);
-// geometry.position = spherePosition;
-// const wireframe = new THREE.WireframeGeometry( geometry );
-// const material = new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('./img/globe.jpg') } );
-const material = new THREE.MeshStandardMaterial({
-    map: new THREE.TextureLoader().load('./img/globe.jpg')
-    })
-
-material.bumpMap   = new THREE.TextureLoader().load('./img/earthbump1k.jpg');     
-material.bumpScale = 0.05;
-
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 30, 30),
-    material
-    );
-scene.add( sphere );
-
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const controls = new OrbitControls( camera, renderer.domElement );
-
-camera.position.z = 3;
-
-const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( ambientLight );
-
-const light = new THREE.PointLight( 0xffffff, 10, 20 );
-light.position.set(10, 10, 10);
-scene.add( light );
+// objects in the scene that we want to access outside of init
+var group;
 
 // convert latitude and longitude coordinates into a useable form
-function calcPosFromLatLonRad(lat, lng) {
-    var phi = (90 - lat) * (Math.PI/180);
-    var theta = (lng + 180) * (Math.PI/180);
-    let x = (-(Math.sin(phi)*Math.cos(theta)));
-    let z = (Math.sin(phi)*Math.sin(theta));
-    let y = (Math.cos(phi));
+function calcPosFromLatLonRad(lat, lng, rad) {
+    // north pole has phi = 0, south pole has phi = PI
+    // but north pole has lat = 90 and south pole has lat = -90
+    // so must convert
+    var phi = (90 - lat) * (Math.PI / 180);
 
-    return {x, y, z};
+    // similarly, must convert longitude to appropriate range for theta
+    var theta = (lng + 180) * (Math.PI / 180);
+
+    // calculate xyz based on angles
+    let x = -(Math.sin(phi) * Math.cos(theta));
+    let z = Math.sin(phi) * Math.sin(theta);
+    let y = Math.cos(phi);
+
+    // multiply so that it has correct radius
+    let vec = new THREE.Vector3(x, y, z).multiplyScalar(rad);
+    return vec;
 }
 
-// add a marker to the globe. NOTE: googled coordinates will NOT show negative signs, so verify if there should be a negative.
-function addMarker(lat, lng, markerColor) {
+function textMesh(s, font, markerColor) {
     let mesh = new THREE.Mesh(
-            new THREE.SphereGeometry(0.025, 20, 20),
-            new THREE.MeshStandardMaterial({ color: markerColor })
-        )
+        new TextGeometry(
+            s,
+            {
+                font: font,
+                size: 0.05,
+                height: 0.01,
+                curveSegments: 10,
+                bevelEnabled: false,
+            }
+        ),
+        new THREE.MeshStandardMaterial({ color: markerColor })
+    )
+    return mesh;
 
-    let pos = calcPosFromLatLonRad(lat, lng)
+}
+// add a marker to the globe
+// NOTE: googled coordinates will be given as degrees N/S and E/W,
+//       but this function expects S and W coordinates to be negative
+//       e.g. (45.4215 N, 75.6972 W) should be passed in as (45.4215, -75.6972)
+function addMarker(name, lat, lng, markerColor, font, group) {
+    let mesh = new THREE.Mesh(
+        new THREE.SphereGeometry(0.025, 40, 40),
+        new THREE.MeshStandardMaterial({ color: markerColor })
+    );
 
-        mesh.position.set(pos.x, pos.y, pos.z)
+    // .copy() sets mesh.position = a copy of the calculated pos vector
+    mesh.position.copy(calcPosFromLatLonRad(lat, lng, 1));
 
-        sphere.add(mesh)
+    let text = textMesh(name, font, markerColor);
+    text.position.copy(calcPosFromLatLonRad(
+        lat - 1,  // move text south a little
+        lng + 2,  //           and east a little
+        1.01      //           and off the earth a little
+    ));
+    text.rotateY((lng + 90) * Math.PI / 180);  // rotate sideways i.e. around equator
+    text.rotateX(-lat * Math.PI / 180);        // rotate vertically i.e. up and down
+
+    console.log(name, lat, lng);
+
+    group.add(mesh)
+    group.add(text)
 }
 
-// Ottawa
-addMarker(45.4215, -75.6972, 0xff0000)
+function init(font) {
+    // basic scene objects
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    document.body.appendChild(renderer.domElement);
 
-// Nairobi
-addMarker(-1.286389, 36.817223, 0x0000ff)
+    // set up camera
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const controls = new OrbitControls(camera, renderer.domElement);
+    camera.position.z = 3;
 
-// Rio de Janeiro
-addMarker(-22.908333, -43.196388, 0x00ff00)
+    // group of all of the objects we want to render in our scene
+    group = new THREE.Group();
+    scene.add(group);
 
+    // globe texture
+    const material = new THREE.MeshStandardMaterial({
+        map: new THREE.TextureLoader().load('./img/globe.jpg')
+    })
+    material.bumpMap = new THREE.TextureLoader().load('./img/earthbump1k.jpg');
+    material.bumpScale = 0.05;
 
+    // globe mesh
+    const sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(1, 20, 20),
+        material
+    );
+    group.add(sphere);
 
+    // lighting
+    const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+    scene.add(ambientLight);
+    const light = new THREE.PointLight(0xffffff, 10, 20);
+    light.position.set(10, 10, 10);
+    scene.add(light);
 
-const group = new THREE.Group()
-group.add(sphere)
-scene.add(group)
+    // add markers to cool cities
+    addMarker('Ottawa', 45.4215, -75.6972, 0xff0000, font, group);
+    addMarker('Nairobi', -1.286389, 36.817223, 0x0000ff, font, group);
+    addMarker('Rio de Janeiro', -22.908333, -43.196388, 0x00ff00, font, group);
 
-const mouse = {
-    x: undefined,
-    y: undefined
+    // start animating
+    animate();
 }
 
 function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-    sphere.rotation.y += 0.005
-    // group.rotation.y = mouse.x * 0.7
-    // group.rotation.x = mouse.y * 0.7
-    
+    // request for animate() to be called again next frame
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+
+    group.rotation.y += 0.005
 }
 
-animate();
-
-
-
-// addEventListener('mousemove', () => {
-//     mouse.x = (event.clientX / innerWidth) * 2 - 1
-//     mouse.y = (event.clientY / innerWidth) * 2 + 1
-// })
+const loader = new FontLoader();
+const font = loader.load(
+    './gentilis_regular.typeface.json',
+    // onLoad
+    function (font) {
+        // init once font is loaded in
+        init(font);
+    },
+    // onProgress (ignore)
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    // onError (ignore)
+    function (err) {
+        console.log('An error happened');
+    }
+);
